431ec4d290d6a7ca90c3cc4a52124bb45b7e1fd3
Vitalii <vitalii.kostiura@gmail.com>
2022-09-02 13:21:52 +0300

    lc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Chart } from './components/Chart/Chart';
 import ChartContainer from './components/Chart/ChartContainer';
 
 function App() {
